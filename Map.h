4e9eafaa02c0c6eb05f458beb1765917a9f509be
西村 龍太 <fko2247017@stu.o-hara.ac.jp>
2022-10-24 11:16:44 +0900

    Mapを作成 カーソルを作成 vectorの使用

---
+++ Map.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include<vector>
+
+class Map
+{
+public:
+	Map();
+	virtual ~Map();
+
+	// グラフィックのロード、アンロード
+	void load();
+	void unload();
+
+	void update();
+
+	// 描画
+	void draw();
+private:
+	// マップチップの数を取得する
+	int chipNumX();
+	int chipNumY();
+	int chipNum();
+private:
+	// マップチップ編集用カーソルの表示
+	void drawCursor();
+	// グラフィックデータのハンドル
+	int m_handle;
+
+	// グラフィックのデータ
+	int m_graphWidth;
+	int m_graphHeight;
+
+	// マップチップ編集用データ
+	int m_cursorNo;
+	std::vector<int> m_mapData;
+};
